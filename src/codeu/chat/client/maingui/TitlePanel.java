e7311b821193f750daa193aab1fd17b54beb986e
Nerissa Lin <lin.nerissa@berkeley.edu>
2017-04-01 15:57:10 -0700

    cleaner title panel

--- src/codeu/chat/client/maingui/TitlePanel.java
+++ src/codeu/chat/client/maingui/TitlePanel.java
@@ -1,17 +1,3 @@
-// Copyright 2017 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package codeu.chat.client.maingui;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -42,13 +28,8 @@ public final class TitlePanel extends JPanel {
     titleLabel.setForeground(Color.decode("#EFF5F7"));
     titleLabel.setFont(new Font("Hoefler Text", Font.BOLD, 36));
     
-    // final JPanel titlePanel = new JPanel(new GridBagLayout());
     this.setBackground(Color.decode("#2A96A5"));
     this.add(titleLabel);
-
-    // final GridBagConstraints titlePanelC = new GridBagConstraints();
-
-    // this.add(titlePanel, titlePanelC);
   }
   
 }
