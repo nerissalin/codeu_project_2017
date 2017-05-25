e0bda5ff96bb963bd62f66d846259a5bd5424e23
Nerissa Lin <lin.nerissa@berkeley.edu>
2017-05-24 23:43:43 -0700

    Updating Conversations Panel to reflect different users

--- src/codeu/chat/client/maingui/UserPanel.java
+++ src/codeu/chat/client/maingui/UserPanel.java
@@ -33,10 +33,12 @@ import codeu.chat.common.User;
 public final class UserPanel extends JPanel {
 
   private final ClientContext clientContext;
+  private final ConversationPanel conversationPanel;
 
-  public UserPanel(ClientContext clientContext) {
+  public UserPanel(ClientContext clientContext, ConversationPanel conversationPanel) {
     super(new GridBagLayout());
     this.clientContext = clientContext;
+    this.conversationPanel = conversationPanel;
     initialize();
   }
 
@@ -155,6 +157,7 @@ public final class UserPanel extends JPanel {
         if (userList.getSelectedIndex() != -1) {
           final String data = userList.getSelectedValue();
           clientContext.user.signInUser(data);
+          conversationPanel.updateButton.doClick();
           userSignedInLabel.setText("Hello " + data);
         }
       }
@@ -167,6 +170,7 @@ public final class UserPanel extends JPanel {
           if (userList.getSelectedIndex() != -1) {
             final String data = userList.getSelectedValue();
             clientContext.user.signInUser(data);
+            conversationPanel.updateButton.doClick();
             userSignedInLabel.setText("Hello " + data);
           }
         }
